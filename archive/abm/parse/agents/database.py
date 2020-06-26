cfd2ef391e17a9245843705f0110369f29094f94
Benjamin Brownlee <benjamin.brownlee1@gmail.com>
2020-06-26 14:37:37 -0700

    cleaning up script archive

--- archive/abm/parse/agents/database.py
+++
@@ -1,6 +0,0 @@
-
-from icarus.util.database import DatabaseUtil
-
-class AgentsParserDatabase(DatabaseUtil):
-    def write_agents(self, agents):
-        self.write_rows(agents, 'agents')
--- archive/network/validate/parcels_in_maz.sql
+++
@@ -1,8 +0,0 @@
-select distinct
-    mazparcels.apn as apn,
-    mazparcels.maz as maz
-from network.mazparcels
-inner join network.mazs
-using(maz)
-where mbrcontains(mazs.region, mazparcels.center) = 0
-limit 20;
